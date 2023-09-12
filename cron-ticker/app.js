a625d4f3b03c16c9d51706e07491a37dd8710827
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2023-09-11 21:23:26 -0500

    rebuild images

--- cron-ticker/app.js
+++ cron-ticker/app.js
@@ -4,5 +4,5 @@ let times = 0;
 
 cron.schedule('*/5 * * * * *', () => {
     times++;
-  console.log('running a task every 5 second', times);
+  console.log('running a task every 5 second :)', times);
 });
